6162aec933aa787b81018fce6e2a661bc831ce68
Yaroslav Chelombitko <les4sixstm@gmail.com>
2024-10-24 13:30:37 +0300

    Update publisher.py

--- Practice02/src/task02/task02/publisher.py
+++ Practice02/src/task02/task02/publisher.py
@@ -27,6 +27,7 @@ class PublisherNode(Node):
 
 def main(args=None):
     rclpy.init(args=args)
+    test_test = len(args.split())
     node = PublisherNode()
 
     # if len(args) > 1:
@@ -41,7 +42,7 @@ def main(args=None):
     # rclpy.shutdown()
 
     try:
-        if len(args) > 1:
+        if test_test > 1:
             node.text = "Hello from test"
         rclpy.spin(node)
     except KeyboardInterrupt:
